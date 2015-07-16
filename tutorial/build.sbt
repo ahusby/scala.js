82fcc4551cc8f25f773c2de3ab20ab83120fa368
arehus <arehus@knowit.no>
2015-07-16 18:48:27 +0200

    Bruker JQuery

--- tutorial/build.sbt
+++ tutorial/build.sbt
@@ -4,4 +4,12 @@ name := "Scala.js Tutorial"
 
 scalaVersion := "2.11.7" // or any other Scala version >= 2.10.2
 
-libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.8.0"
+// Bruker JQuery istedenfor DOM direkte
+libraryDependencies += "be.doeraene" %%% "scalajs-jquery" % "0.8.0"
+
+// The Scala.js sbt plugin provides a mechanism for libraries to declare the plain JavaScript libraries they depend on
+// and bundle them in a single file. All you have to do is activate this and then include the file in the HTML.
+// After reloading and rerunning fastOptJS, this will create scala-js-tutorial-jsdeps.js containing all JavaScript
+// libraries next to the main JavaScript file. We can then simply include this file and don't need to worry about
+// JavaScript libraries anymore.
+skip in packageJSDependencies := false
--- tutorial/src/main/scala/tutorial/webapp/TutorialApp.scala
+++ tutorial/src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -1,25 +1,20 @@
 package tutorial.webapp
 
-import scala.scalajs.js.JSApp
-import org.scalajs.dom
-import dom.document
+import org.scalajs.jquery.jQuery
 
-import scala.scalajs.js.annotation.JSExport
+import scala.scalajs.js.JSApp
 
 object TutorialApp extends JSApp {
   def main(): Unit = {
-    appendPar(document.body, "Hello World")
+    jQuery(setupUI _)
   }
 
-  def appendPar(targetNode: dom.Node, text: String): Unit = {
-    val parNode = document.createElement("p")
-    val textNode = document.createTextNode(text)
-    parNode.appendChild(textNode)
-    targetNode.appendChild(parNode)
+  def setupUI(): Unit = {
+    jQuery("#click-me-button").click(addClickedMessage _)
+    jQuery("body").append("<p>Hello World</p>")
   }
 
-  @JSExport
   def addClickedMessage(): Unit = {
-    appendPar(document.body, "You clicked the button!")
+    jQuery("body").append("<p>Button was clicked</p>")
   }
 }
